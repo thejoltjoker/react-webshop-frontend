03170dc2699f39026e0bd845312f7886134fbdcd
Johannes Andersson <hello@thejoltjoker.com>
2024-02-02 12:57:16 +0000

    Try router with basename

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,30 +1,33 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import ErrorNotFound from "../pages/ErrorNotFound";
 import Home from "../pages/Home";
 import MainLayout from "../pages/MainLayout";
-import Products from "../pages/Products";
-import ErrorNotFound from "../pages/ErrorNotFound";
 import ProductPage from "../pages/ProductPage";
+import Products from "../pages/Products";
 
-export const router = createHashRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout />,
 
-    errorElement: <ErrorNotFound />,
-    children: [
-      {
-        path: "/",
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/products",
-        element: <Products />,
-      },
-      {
-        path: "/product/:slug",
-        element: <ProductPage />,
-      },
-    ],
-  },
-]);
+      errorElement: <ErrorNotFound />,
+      children: [
+        {
+          path: "/",
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/product/:slug",
+          element: <ProductPage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/react-webshop-frontend/" },
+);
