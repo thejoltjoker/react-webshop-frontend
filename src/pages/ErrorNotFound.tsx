e50eeea9bb4cb245a2cd4b4e0f8fcbe0a2fbc3aa
Johannes Andersson <hello@thejoltjoker.com>
2024-02-02 12:58:42 +0000

    Add error page

--- src/pages/ErrorNotFound.tsx
+++ src/pages/ErrorNotFound.tsx
@@ -6,7 +6,7 @@ const ErrorNotFound = () => {
     <>
       <Navbar />
 
-      <div className="relative flex max-h-[80vh] min-h-40 w-full overflow-hidden object-cover">
+      <div className="relative flex h-[70vh] w-full overflow-hidden object-cover">
         <div className="absolute flex h-full w-full flex-col items-center justify-center text-center">
           <h2 className=" mb-8 text-6xl text-white">Are you lost?</h2>
           <p className=" text-xl font-light uppercase tracking-widest text-white opacity-50">
@@ -14,9 +14,9 @@ const ErrorNotFound = () => {
           </p>
         </div>
         <img
-          src="./img/gantas-vaiciulenas-FVz5fDfGFFw-unsplash.jpg"
+          src="/react-webshop-frontend/img/404.jpg"
           alt=""
-          className="self-center"
+          className="absolute bottom-0 -z-10"
         />
       </div>
 
